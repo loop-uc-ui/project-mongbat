4c46595ea526246b43bf6fdebf5d67da4d3d8702
loop-uc-ui <loopucui@gmail.com>
2025-11-22 00:27:06 -0500

    remove passing of context

--- src/mods/mongbat-main-menu/MongbatMainMenuMod.lua
+++ src/mods/mongbat-main-menu/MongbatMainMenuMod.lua
@@ -1,116 +1,105 @@
----@param context Context
----@return Button
-local Button = function (context, text, onLButtonUp, onShown)
-    return context.Components.Button {
-        OnLButtonUp = onLButtonUp,
-
-        OnInitialize = function (self)
-            self:setDimensions(130, 41)
-            self:setText(text)
-            if onShown ~= nil then
-                onShown(self)
-            end
-        end
-    }
-end
-
----@param context Context
-local Window = function (context)
-    return context.Components.Window {
-        Name = "MainMenuWindow",
-        ---@param self Window
-        OnInitialize = function (self)
-            self:setDimensions(214, 440)
-            self:anchorToParentCenter()
-            self:setChildren {
-                Button(
-                    context,
-                    3000128,
-                    function ()
-                        context.Api.Event.Logout()
-                    end,
-                    ---@param button Button
-                    function (button)
-                        button:anchorToParentTop()
-                    end
-                ),
-                Button(
-                    context,
-                    1077859,
-                    function ()
-                        context.Api.Event.ExitGame()
-                    end
-                ),
-                Button(
-                    context,
-                    L"Settings",
-                    function ()
-                        context.Api.Window.ToggleWindow("SettingsWindow")
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    L"Store",
-                    function ()
-                        context.Api.Event.OpenStore()
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    L"Agents",
-                    function ()
-                        context.Api.Window.ToggleWindow("OrganizerWindow")
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    3000172,
-                    function ()
-                        context.Api.Window.ToggleWindow("MacroWindow")
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    1079812,
-                    function ()
-                        context.Api.Window.ToggleWindow("ActionsWindow")
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    1061037,
-                    function ()
-                        context.Api.Event.OpenHelpMenu()
-                        self:setShowing(false)
-                    end
-                ),
-                Button(
-                    context,
-                    L"Debug",
-                    function ()
-                        context.Api.Window.ToggleWindow("DebugWindow")
-                        self:setShowing(false)
-                    end
-                )
-            }
-        end,
-        OnRButtonUp = function (self)
-            self:setShowing(false)
-        end
-    }
-end
-
-
-MongbatMainMenuMod = Mongbat.Mod {
+Mongbat.Mod {
     Name = "MongbatMainMenu",
     Path = "/src/mods/mongbat-main-menu",
     OnInitialize = function (context)
-        context.Components.Defaults.MainMenuWindow:asComponent():destroy()
-        Window(context):create(false)
+        local default = context.Components.Defaults.MainMenuWindow:asComponent()
+
+        local Button = function (text, onLButtonUp, onShown)
+            return context.Components.Button {
+                OnLButtonUp = onLButtonUp,
+
+                OnInitialize = function (self)
+                    self:setDimensions(130, 41)
+                    self:setText(text)
+                    if onShown ~= nil then
+                        onShown(self)
+                    end
+                end
+            }
+        end
+
+        local Window = function ()
+            return context.Components.Window {
+                Name = default:getName(),
+                ---@param self Window
+                OnInitialize = function (self)
+                    self:setDimensions(214, 440)
+                    self:anchorToParentCenter()
+                    self:setChildren {
+                        Button(
+                            3000128,
+                            function ()
+                                context.Api.Event.Logout()
+                            end,
+                            ---@param button Button
+                            function (button)
+                                button:anchorToParentTop()
+                            end
+                        ),
+                        Button(
+                            1077859,
+                            function ()
+                                context.Api.Event.ExitGame()
+                            end
+                        ),
+                        Button(
+                            L"Settings",
+                            function ()
+                                context.Api.Window.ToggleWindow("SettingsWindow")
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            L"Store",
+                            function ()
+                                context.Api.Event.OpenStore()
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            L"Agents",
+                            function ()
+                                context.Api.Window.ToggleWindow("OrganizerWindow")
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            3000172,
+                            function ()
+                                context.Api.Window.ToggleWindow("MacroWindow")
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            1079812,
+                            function ()
+                                context.Api.Window.ToggleWindow("ActionsWindow")
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            1061037,
+                            function ()
+                                context.Api.Event.OpenHelpMenu()
+                                self:setShowing(false)
+                            end
+                        ),
+                        Button(
+                            L"Debug",
+                            function ()
+                                context.Api.Window.ToggleWindow("DebugWindow")
+                                self:setShowing(false)
+                            end
+                        )
+                    }
+                end,
+                OnRButtonUp = function (self)
+                    self:setShowing(false)
+                end
+            }
+        end
+
+        default:destroy()
+        Window():create(false)
     end
 }
--- src/mods/mongbat-player-status/MongbatPlayerStatusMod.lua
+++ src/mods/mongbat-player-status/MongbatPlayerStatusMod.lua
@@ -1,68 +1,65 @@
 
 
-local NAME = "StatusWindow"
-
----@param context Context
----@param name string
----@param onUpdatePlayerStatus fun(self: StatusBar, playerStatus: PlayerStatusWrapper)
-local function StatusBar(context, name, onUpdatePlayerStatus)
-    return context.Components.StatusBar {
-        Name = NAME .. name,
-        ---@param self StatusBar
-        OnInitialize = function (self)
-            self:setDimensions(300, 24)
-        end,
-        OnUpdatePlayerStatus = onUpdatePlayerStatus
-    }
-end
-
-local function HealthStatusBar(context)
-    return StatusBar(context, "HealthBar", function (self, playerStatus)
-        self:setCurrentValue(playerStatus:getCurrentHealth())
-        self:setMaxValue(playerStatus:getMaxHealth())
-    end)
-end
-
-local function ManaStatusBar(context)
-    return StatusBar(context, "ManaBar", function (self, playerStatus)
-        self:setCurrentValue(playerStatus:getCurrentMana())
-        self:setMaxValue(playerStatus:getMaxMana())
-    end)
-end
-
-local function StaminaStatusBar(context)
-    return StatusBar(context, "StaminaBar", function (self, playerStatus)
-        self:setCurrentValue(playerStatus:getCurrentStamina())
-        self:setMaxValue(playerStatus:getMaxStamina())
-    end)
-end
-
----@param context Context
-local function Window(context)
-    return context.Components.Window {
-        Name = NAME,
-        PersistPosition = true,
-        ---@param self Window
-        OnInitialize = function (self)
-            self:setDimensions(300, 150)
-            self:setChildren {
-                HealthStatusBar(context),
-                ManaStatusBar(context),
-                StaminaStatusBar(context)
-            }
-        end,
-        OnRButtonUp = function (_)
-        end
-    }
-end
-
-MongbatPlayerStatusMod = Mongbat.Mod {
+Mongbat.Mod {
     Name = "MongbatPlayerStatus",
     Path = "/src/mods/mongbat-player-status",
     OnInitialize = function (context)
         local original = context.Components.Defaults.StatusWindow
+        local statusWindow = original:getName()
+
+        ---@param name string
+        ---@param onUpdatePlayerStatus fun(self: StatusBar, playerStatus: PlayerStatusWrapper)
+        local function StatusBar(name, onUpdatePlayerStatus)
+            return context.Components.StatusBar {
+                Name = statusWindow .. name,
+                ---@param self StatusBar
+                OnInitialize = function (self)
+                    self:setDimensions(300, 24)
+                end,
+                OnUpdatePlayerStatus = onUpdatePlayerStatus
+            }
+        end
+
+        local function HealthStatusBar()
+            return StatusBar("HealthBar", function (self, playerStatus)
+                self:setCurrentValue(playerStatus:getCurrentHealth())
+                self:setMaxValue(playerStatus:getMaxHealth())
+            end)
+        end
+
+        local function ManaStatusBar()
+            return StatusBar("ManaBar", function (self, playerStatus)
+                self:setCurrentValue(playerStatus:getCurrentMana())
+                self:setMaxValue(playerStatus:getMaxMana())
+            end)
+        end
+
+        local function StaminaStatusBar()
+            return StatusBar("StaminaBar", function (self, playerStatus)
+                self:setCurrentValue(playerStatus:getCurrentStamina())
+                self:setMaxValue(playerStatus:getMaxStamina())
+            end)
+        end
+
+        local function Window()
+            return context.Components.Window {
+                Name = statusWindow,
+                PersistPosition = true,
+                ---@param self Window
+                OnInitialize = function (self)
+                    self:setDimensions(300, 150)
+                    self:setChildren {
+                        HealthStatusBar(),
+                        ManaStatusBar(),
+                        StaminaStatusBar()
+                    }
+                end,
+                OnRButtonUp = function (_) end
+            }
+        end
+
         original:getDefault().UpdateLatency = function () end
         original:asComponent():destroy()
-        Window(context):create(true)
+        Window():create(true)
     end
 }
