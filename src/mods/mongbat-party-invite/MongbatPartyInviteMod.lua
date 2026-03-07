7874dbe7f6fe8c18490f09488ea321994c1ff82f
loop-uc-ui <loopucui@gmail.com>
2026-03-06 21:15:33 -0500

    remove context as a concept

--- src/mods/mongbat-party-invite/MongbatPartyInviteMod.lua
+++ src/mods/mongbat-party-invite/MongbatPartyInviteMod.lua
@@ -1,13 +1,16 @@
 local NAME = "MongbatPartyInviteWindow"
+local Api = Mongbat.Api
+local Data = Mongbat.Data
+local Components = Mongbat.Components
+local Constants = Mongbat.Constants
 
 -- File-scope: needed by both OnInitialize and OnShutdown to restore overridden functions
 local originalInitialize
 local originalShutdown
 local originalCloseWindow
 
----@param context Context
-local function OnInitialize(context)
-    local default = context.Components.Defaults.PartyInviteWindow
+local function OnInitialize()
+    local default = Components.Defaults.PartyInviteWindow
     local doNotShowChecked = false
 
     -- Save originals before overriding so OnShutdown can restore them
@@ -16,59 +19,59 @@ local function OnInitialize(context)
     originalCloseWindow = default:getDefault().CloseWindow
 
     local function destroyWindow()
-        context.Api.Window.Destroy(NAME)
+        Api.Window.Destroy(NAME)
     end
 
     local function createWindow()
         doNotShowChecked = false
-        local inviterName = context.Data.PartyInviteName()
-        local dialogText = inviterName .. context.Api.String.GetStringFromTid(1115373)
+        local inviterName = Data.PartyInviteName()
+        local dialogText = inviterName .. Api.String.GetStringFromTid(1115373)
 
-        context.Components.Window {
+        Components.Window {
             Name = NAME,
             Resizable = false,
             OnRButtonUp = function()
-                context.Api.Window.Destroy("PartyInviteWindow")
+                Api.Window.Destroy("PartyInviteWindow")
             end,
             OnInitialize = function(self)
                 self:setDimensions(350, 165)
                 self:anchorToParentCenter()
                 self:setChildren {
-                    context.Components.Label {
+                    Components.Label {
                         OnInitialize = function(self)
                             self:setDimensions(310, 50)
                             self:setText(dialogText)
                             self:centerText()
                         end
                     },
-                    context.Components.Button {
+                    Components.Button {
                         OnInitialize = function(self)
-                            self:setText(context.Api.String.GetStringFromTid(1115371))
+                            self:setText(Api.String.GetStringFromTid(1115371))
                         end,
                         OnLButtonUp = function()
-                            context.Api.Party.AcceptInvite()
-                            context.Api.Window.Destroy("PartyInviteWindow")
+                            Api.Party.AcceptInvite()
+                            Api.Window.Destroy("PartyInviteWindow")
                         end
                     },
-                    context.Components.Button {
+                    Components.Button {
                         OnInitialize = function(self)
-                            self:setText(context.Api.String.GetStringFromTid(1115372))
+                            self:setText(Api.String.GetStringFromTid(1115372))
                         end,
                         OnLButtonUp = function()
-                            context.Api.Party.DeclineInvite()
-                            context.Api.Window.Destroy("PartyInviteWindow")
+                            Api.Party.DeclineInvite()
+                            Api.Window.Destroy("PartyInviteWindow")
                         end
                     },
-                    context.Components.Button {
+                    Components.Button {
                         OnInitialize = function(self)
-                            self:setText(context.Api.String.GetStringFromTid(1115374))
+                            self:setText(Api.String.GetStringFromTid(1115374))
                         end,
                         OnLButtonUp = function(self)
                             doNotShowChecked = not doNotShowChecked
                             if doNotShowChecked then
-                                self:setText(L"[x] " .. context.Api.String.GetStringFromTid(1115374))
+                                self:setText(L"[x] " .. Api.String.GetStringFromTid(1115374))
                             else
-                                self:setText(context.Api.String.GetStringFromTid(1115374))
+                                self:setText(Api.String.GetStringFromTid(1115374))
                             end
                         end
                     }
@@ -78,9 +81,9 @@ local function OnInitialize(context)
 
         -- Mirror the default UI's window-level CLOSE_PARTY_INVITE registration so the
         -- engine signal reaches CloseWindow even though we skipped the original Initialize.
-        context.Api.Window.RegisterEventHandler(
+        Api.Window.RegisterEventHandler(
             "PartyInviteWindow",
-            context.Constants.Broadcasts.ClosePartyInvite(),
+            Constants.Broadcasts.ClosePartyInvite(),
             "PartyInviteWindow.CloseWindow"
         )
     end
@@ -90,11 +93,11 @@ local function OnInitialize(context)
     end
 
     default:getDefault().Shutdown = function()
-        if context.Api.Party.IsRequestPending() then
-            context.Api.Party.DeclineInvite()
+        if Api.Party.IsRequestPending() then
+            Api.Party.DeclineInvite()
         end
         if doNotShowChecked then
-            context.Api.Party.SetShowInvitePopUp(false)
+            Api.Party.SetShowInvitePopUp(false)
         end
         destroyWindow()
     end
@@ -102,19 +105,18 @@ local function OnInitialize(context)
     -- CloseWindow is called by the CLOSE_PARTY_INVITE engine signal (registered above in
     -- createWindow). Destroying "PartyInviteWindow" triggers the overridden Shutdown.
     default:getDefault().CloseWindow = function()
-        context.Api.Window.Destroy("PartyInviteWindow")
+        Api.Window.Destroy("PartyInviteWindow")
     end
 end
 
----@param context Context
-local function OnShutdown(context)
-    local default = context.Components.Defaults.PartyInviteWindow
+local function OnShutdown()
+    local default = Components.Defaults.PartyInviteWindow
 
     -- If a party invite is showing, destroy the XML window first so the overridden Shutdown
     -- fires (declining the pending invite and applying the "do not show" setting) before we
     -- restore the originals.
-    context.Api.Window.Destroy("PartyInviteWindow")
-    context.Api.Window.Destroy(NAME)
+    Api.Window.Destroy("PartyInviteWindow")
+    Api.Window.Destroy(NAME)
 
     -- Restore the functions we overwrote on the original table.
     default:getDefault().Initialize = originalInitialize
